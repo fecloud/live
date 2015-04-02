4f378197b6d54cf048f4ec62c3998a9328f73af7
fecloud <oyf_feng@163.com>
2015-04-02 23:55:48 +0800

    add randmo str

--- src/exe/vlrtmp.cpp
+++ src/exe/vlrtmp.cpp
@@ -7,24 +7,61 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <sstream>
+
+#include "Poco/Base64Encoder.h"
+#include "Poco/Base64Decoder.h"
+#include "Poco/Exception.h"
 
 #include "h264/NetH264Reader.h"
 #include "rtmp/RTMPOutPutStream.h"
 #include "encoder/FLVEncoder.h"
 
 using namespace std;
+using namespace Poco;
+
+/**
+ * 创建随机字符串
+ */
+string randomstr() {
+	srand((unsigned) time(NULL));
+	const int r = rand();
+	std::ostringstream str;
+	std::stringstream instr;
+	instr << r;
+	Base64Encoder en(str);
+	en << instr.str();
+	en.close();
+	const int len = str.str().length();
+	return str.str().replace(len - 2, 2, "XX");
+}
+
+bool write_url(const char *url) {
+	FILE *fp = fopen("/var/run/liveurl", "w+");
+	if (!fp) {
+		cout << "open /var/run/liveurl fail" << endl;
+		return false;
+	}
+	fwrite(url, strlen(url), 1, fp);
+	fclose(fp);
+	fp = NULL;
+	return true;
+}
 #ifndef NO_MAIN
-int main(int argc, char **argv)
-{
-	if (argc < 3)
-	{
+int main(int argc, char **argv) {
+	if (argc < 3) {
 		cout << "Uage:" << argv[0] << " <src server> <rtmp server>" << endl;
 		exit(0);
 	}
 	NetH264Reader reader(argv[1]);
 
-	RTMPOutPutStream output(argv[2], true);
-	FLVEncoder encoder(&reader, &output);
-	encoder.encoder();
+	std::stringstream instr;
+	instr << argv[2];
+	instr << randomstr();
+	if (write_url(instr.str().c_str())) {
+		RTMPOutPutStream output(instr.str().c_str(), true);
+		FLVEncoder encoder(&reader, &output);
+		encoder.encoder();
+	}
 }
 #endif
